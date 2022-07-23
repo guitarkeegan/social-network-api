6f1d8ac29576990fdbd533a3c509c4e9479fbe10
guitarkeegan <keegananglim@gmail.com>
2022-07-23 08:14:41 -0700

    add date format for reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,35 @@
+const { Schema, Types } = require('mongoose');
+const dayjs = require('dayjs');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => {
+        if (date) return dayjs(date).format('MM-DD-YYYY h:mma')
+      },
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
