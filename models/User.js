f6006fdf75a49aaa950fab085edc71272e859b6e
guitarkeegan <keegananglim@gmail.com>
2022-07-23 07:58:40 -0700

    refactor user model by adding ref fields to friends and thoughts

--- models/User.js
+++ models/User.js
@@ -1,21 +1,43 @@
 const { Schema, model } = require('mongoose');
 const thoughtsSchema = require('./Thought')
 
+let validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
 // Schema to create User model
 const userSchema = new Schema(
   {
     username: {
       type: String,
       required: true,
+      unique: true,
       max_length: 50,
+      trim: true
     },
     email: {
       type: String,
       required: true,
+      unique: true,
       max_length: 50,
+      trim: true,
+      lowercase: true,
+      validate: [validateEmail, 'Please fill a valid email address'],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,5})+$/, 'Please fill a valid email address']
     },
-    thoughts: [thoughtsSchema],
-    friends: [userSchema]
+    thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'thought',
+        },
+      ],
+    friends: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+    },
+    ]
   },
   {
     toJSON: {
@@ -24,7 +46,7 @@ const userSchema = new Schema(
     },
   }
 );
-
+// access the friend array to get the number of friends
 userSchema.virtual('friendCount')
 .get(function(){
     return this.friends.length;
