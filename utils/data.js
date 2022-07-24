c4a0121c5b1dd2b8951255ab418ac1eec40414c1
guitarkeegan <keegananglim@gmail.com>
2022-07-24 11:28:25 -0700

    refactor user and thought seeds

--- utils/data.js
+++ utils/data.js
@@ -74,46 +74,58 @@ const names = [
     'Parker',
   ];
   
-  const appDescriptions = [
-    'Decision Tracker',
-    'Find My Phone',
-    'Learn Piano',
-    'Starbase Defender',
-    'Tower Defense',
-    'Monopoly Money Manager',
-    'Movie trailers',
-    'Hello world',
+  const thoughts = [
+    `I've got decisions to make.`,
+    'Had anyone seen a phone that can take two photos at once?',
+    'Learning piano is difficult.',
+    'Startroopers was a great movie!',
+    'Towers are good for defense',
+    'Monopoly money manager I am.',
+    'Movie trailers are the best part of movies.',
+    'Hello world is what we say.',
     'Stupid Social Media App',
-    'Notes',
-    'Messages',
-    'Email',
-    'Compass',
-    'Firefox',
-    'Running app',
-    'Cooking app',
-    'Poker',
-    'Deliveries',
+    'Notes tell us what to remember.',
+    'Messages is a great app. Haters gonna hate.',
+    'Email is so old school. Does anyone use it?',
+    'Compass, has its pros and cons for sure.',
+    'Firefox is still the ultimate browser.',
+    'My running app keeps telling me to slow down... haha',
+    'My new cooking app is ruining my diet.',
+    `Poker players! Game starts at 6!`,
+    `Has anyone tried the Amazon drone delivery service?`,
   ];
   
   // Get a random item given an array
   const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+  // Gets a random full name and email
+  const getRandomName = () => {
+    const randNumbers = Math.floor(Math.random() * 4000);
+    return `${getRandomArrItem(names)}${randNumbers}${getRandomArrItem(names)}`
+  }
   
-  // Gets a random full name
-  const getRandomName = () =>
-    `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
-  
-  // Function to generate random assignments that we can add to student object.
-  const getRandomAssignments = (int) => {
+  // Function to generate random thoughts that we can add to user object.
+  const getRandomThoughts = (int) => {
     const results = [];
     for (let i = 0; i < int; i++) {
       results.push({
-        assignmentName: getRandomArrItem(appDescriptions),
-        score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
+        randomThought: getRandomArrItem(thoughts),
       });
     }
     return results;
   };
+
+  const getRandomEmail = (int) => {
+    const results = [];
+    for (let i=0;i<int;i++){
+        const randNumbers = Math.floor(Math.random() * 4000);
+        const emailEnding = ['com', 'net', 'org', 'edu', 'gov', 'io']
+        results.push(`randoemail${randNumbers}@${getRandomArrItem(emailEnding)}`);
+    }
+  }
+
+  
   
   // Export the functions for use in seed.js
-  module.exports = { getRandomName, getRandomAssignments };
+  module.exports = { getRandomName, getRandomThoughts, getRandomEmail };
   
--- utils/seed.js
+++ utils/seed.js
@@ -1,6 +1,6 @@
 const connection = require('../config/connection');
 const { Thought, User } = require('../models');
-const { getRandomName, getRandomAssignments } = require('./data');
+const { getRandomName, getRandomThoughts, getRandomEmail } = require('./data');
 
 connection.on('error', (err) => err);
 
@@ -8,29 +8,25 @@ connection.once('open', async () => {
   console.log('connected');
 
   // Drop existing courses
-  await Course.deleteMany({});
+  await Thought.deleteMany({});
 
   // Drop existing students
-  await Student.deleteMany({});
+  await User.deleteMany({});
 
-  // Create empty array to hold the students
-  const students = [];
+  // Create empty array to hold the users
+  const users = [];
 
   // Loop 20 times -- add students to the students array
   for (let i = 0; i < 20; i++) {
-    // Get some random assignment objects using a helper function that we imported from ./data
-    const assignments = getRandomAssignments(20);
+    // Get some random thought objects using a helper function that we imported from ./data
+    const thoughts = getRandomThoughts(20);
+    const email = getRandomEmail(20)
+    const username = getRandomName();
 
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
-
-    students.push({
-      first,
-      last,
-      github,
-      assignments,
+    users.push({
+      username,
+      thoughts,
+      email,
     });
   }
 
