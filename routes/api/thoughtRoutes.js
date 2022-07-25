c194d9a249fb0f693665b776d86e2b93b60d7305
guitarkeegan <keegananglim@gmail.com>
2022-07-25 08:15:30 -0700

    update userController friend methods

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,22 +1,22 @@
 const router = require('express').Router();
-const { builtinModules } = require('module');
+
 const {
-  getAllThoughts,
+  getThoughts,
   getSingleThought,
   createThought,
   updateThought,
   deleteThought,
-  createReaction,
-  deleteReaction
-} = require('../../controllers/userController');
+  addReaction,
+  removeReaction
+} = require('../../controllers/thoughtController');
 
 // /api/thoughts
-router.route('api/thoughts').get(getAllThoughts).post(createThought);
+router.route('api/thoughts').get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
 router.route('api/thoughts/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
-router.route('api/thoughts/:thoughtId/reations').post(createReaction).delete(deleteReaction);
+router.route('api/thoughts/:thoughtId/reations').post(addReaction).delete(removeReaction);
 
 module.exports = router;
